3d9d8c1b3e111d9d7bd613b7943b7484d5435873
phamdatqn <phamdat.qn@gmail.com>
2022-08-08 21:55:01 +0700

    fix exercise

--- ss01_co_so_du_lieu_quan_he/exercise/create_table_student.sql
+++ ss01_co_so_du_lieu_quan_he/exercise/create_table_student.sql
@@ -3,19 +3,19 @@ CREATE DATABASE stundent_management;
 USE stundent_management;
 
 CREATE TABLE student (
-    idStudent INT PRIMARY KEY NOT NULL,
+    idStudent INT PRIMARY KEY,
     `name` VARCHAR(45),
     age INT,
     country VARCHAR(45)
 );
 
 CREATE TABLE class (
-    idClass INT PRIMARY KEY NOT NULL,
+    idClass INT PRIMARY KEY,
     `name` VARCHAR(45)
 );
 
 CREATE TABLE teacher (
-    idTeacher INT PRIMARY KEY NOT NULL,
+    idTeacher INT PRIMARY KEY,
     `name` VARCHAR(45),
     age INT,
     country VARCHAR(45)
